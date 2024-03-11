630b3ae596f394c03909c250729976492d6342bd
Matias Sanchez <matiasanchez999@gmail.com>
2024-03-11 00:15:44 -0300

    creation of the header component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
-import NavBar from '../../components/navbar/NavBar'
 import './home.css'
+import Header from '../../components/header/Header'
+import NavBar from '../../components/navbar/NavBar'
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Header />
     </div>
   )
 }
